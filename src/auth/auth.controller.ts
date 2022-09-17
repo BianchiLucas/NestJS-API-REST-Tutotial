acba94e5f5c1f6a978be121418027ff512e0a172
BianchiLucas <lucasbianchi92@gmail.com>
2022-09-17 17:19:44 -0300

    Auth controller modile services and Docker compose

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    //Endpoints
+    @Post('signup')
+    signup() {
+        return this.authService.signup()
+    }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin()
+    }
+}
+//pivate reemplaza this.authService = authService
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class AuthService {
+    //Funciones 
+    signup() {
+        return {
+            msg: 'I have signed up'
+        }
+    }
+
+    signin() {
+        return {
+            msg: 'I have signed in'
+        }
+    }
+}
