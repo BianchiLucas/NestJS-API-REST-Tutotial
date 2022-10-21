2cc16ffa68b6be5b48ddef1bf095ce74a657fc82
BianchiLucas <lucasbianchi92@gmail.com>
2022-10-20 22:09:09 -0300

    Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookmarks project')
+    .setDescription('Bookmarks project using nestjs, Prisma db with Docker compose, jwt, passport y e2e tests whit pactum')
+    .setVersion('1.0.0')
+    .addTag('nestjs')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
